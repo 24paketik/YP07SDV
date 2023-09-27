fc42ea7a584be080ea91c1c2514197178e7eec50
24paketik <87018761+24paketik@users.noreply.github.com>
2023-09-27 15:32:04 +0300

    update

---
+++ Wpf/AttachedProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf
+{
+    internal class AttachedProduct
+    {
+        public int MainProductId { get; set; }
+        public int AttachedProductID { get; set; }
+    }
+}
---
+++ Wpf/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Wpf
+{
+    internal class ApplicationContext:DbContext
+    {
+        public DbSet<AttachedProduct> attachedProducts { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=46.42.17.183;Database=St2_Smirnov_Salon1;Trusted_Connection=True;");
+        }
+    }
+}
